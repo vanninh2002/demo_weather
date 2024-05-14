874f375fa7173b47603af5d3020d2986a3d05928
vanninh2002 <pioktwo@gmail.com>
2024-05-14 21:41:32 +0700

    Add project files.

---
+++ clone/demo_api_57kmt/api.aspx.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace demo_api_57kmt
+{
+    public partial class api : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-4TQI2HI\\SQLEXPRESS;Initial Catalog=test;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SP_Chart", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    try
+                    {
+                        connection.Open();
+                        object kq = command.ExecuteScalar();
+                        string json = (string)kq;
+                        this.Response.ContentType = "application/json";
+                        this.Response.Write(json);
+                    }
+                    catch (Exception ex)
+                    {
+                        this.Response.ContentType = "application/json";
+                        this.Response.Write("{\"ok\":0,\"msg\":\"" + ex.Message + "\"}");
+                    }
+                }
+            }
+        }
+    }
+}
